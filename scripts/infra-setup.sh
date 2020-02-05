b51a84a8798b4d65366c6aa9cce9d0cb9b63554a
edaena <edaena@outlook.com>
2020-02-05 13:33:48 -0800

    Change script output, add environment instructions

--- scripts/infra-setup.sh
+++ scripts/infra-setup.sh
@@ -12,22 +12,26 @@ user_alias=$(az ad signed-in-user show | jq '.mailNickname' -r)
 
 sa=$user_alias'spkinfrasa'
 sac='infracontainer'
+cluster_name=$user_alias'-spk-cluster'
 
 echo "Creating resources in subscription: $subscription"
 echo "Get user alias..."
 
 echo "User alias: $user_alias"
 
+rgi=$user_alias'-spkinfra-rg'
 rg=$user_alias'-spkcluster-rg'
 echo "Create resource group: $rg"
 #resource_group=$(az group create -l westus -n $rg)
+
+echo "Create resource group: $rgi"
+#resource_group=$(az group create -l westus -n $rgi)
 
 echo "Create service principal"
 #> service-principal.json
 #sp=$(az ad sp create-for-rbac --scopes "/subscriptions/"$id >> service-principal.json)
 
 dir=$(pwd)
-echo "Resource group: $rg"
 echo "Created service principal at $dir/service-principal.json"
 
 # Create storage account
@@ -53,10 +57,15 @@ ssh-keygen -b 4096 -t rsa -f cluster-deployment/keys/node-ssh-key
 # Save values in a file:
 > infra-values.txt
 echo "Subscription id: $id" >> infra-values.txt
-echo "Resource group name: $rg" >> infra-values.txt
-echo "Storage account name: $sa" >> infra-values.txt
-echo "Storage account key: $a_key" >> infra-values.txt
-echo "Container name: $sac" >> infra-values.txt
+echo "cluster_name: $cluster_name" >> infra-values.txt
+echo "dns_prefix: $cluster_name" >> infra-values.txt
+echo "resource_group_name: $rg" >> infra-values.txt
+echo "global_resource_group_name (common infra): $rgi" >> infra-values.txt
+echo "keyvault_resource_group: $rgi" >> infra-values.txt
+echo "storage_account_name: $sa" >> infra-values.txt
+echo "access_key (for storage account): $a_key" >> infra-values.txt
+echo "container_name: $sac" >> infra-values.txt
+echo "gitops_path: $dir/cluster-deployment/keys/gitops-ssh-key" >> infra-values.txt
 echo "" >> infra-values.txt
 echo "Deploy key path: $dir/cluster-deployment/keys/gitops-ssh-key" >> infra-values.txt
 echo "Deploy public key file: $dir/cluster-deployment/keys/gitops-ssh-key.pub" >> infra-values.txt
